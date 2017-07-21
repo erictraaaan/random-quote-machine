61a96e47604f6402156707eee1d4ae241bf4fe92
Eric Tran <trane3@mcmaster.ca>
2017-07-20 21:56:58 -0400

    different way to get quote

--- script.js
+++ script.js
@@ -1,42 +1,61 @@
 $(document).ready(function(){
-
-	var quote;
-	var author;
-
-	function getNewQuote(){
-		$.ajax({ //for making api requests
-			url: 'http://api.forismatic.com/api/1.0/',
-			jsonp: 'jsonp',
-			dataType: 'jsonp',
-			data: {
-				method: 'getQuote',
-				lang: 'en',
-				format: 'jsonp'
-			}, // this is equivalent to url: http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en
-			success: function(response){
-				quote = response.quoteText;
-				author = response.quoteAuthor;
-				$('#quote').text(quote);
-				if (author){
-					$('#author').text('- ' + author);
-				} else {
-					$('#author').text('- Unknown');
-				}
-			}
-		});
-	}
-	
 	getNewQuote();
 
-	$('.get-quote').on('click', function(event){
-		event.preventDefault();
-		getNewQuote();
-	});
+	// var quote;
+	// var author;
 
-	$('.share-quote').on('click', function(event){
-		event.preventDefault();
-		window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote + ' -- ' + author));
-	});
+	function getNewQuote(){
+		var url = "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en"
+
+		$.getJSON(url, function(data){
+			var quote = data.quoteText;
+			var author = data.quoteAuthor;
+			if (author === ''){
+				author = 'Unknown';
+			}
+
+			$("#quote").html('"' + quote + '"');
+			$("#author").html("- " + author);
+			$('.get-quote').on("click", function(){
+				getNewQuote();
+			});
+
+		});
+
+
+		// $.ajax({ //for making api requests
+		// 	url: 'https://cors-anywhere.kerokuapp.com/http://api.forismatic.com/api/1.0/',
+		// 	// jsonp: 'jsonp',
+		// 	dataType: 'json',
+		// 	data: {
+		// 		method: 'getQuote',
+		// 		lang: 'en',
+		// 		format: 'json'
+		// 	}, // this is equivalent to url: http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en
+		// 	success: function(response){
+		// 		quote = response.quoteText;
+		// 		author = response.quoteAuthor;
+		// 		$('#quote').text(quote);
+		// 		if (author){
+		// 			$('#author').text('- ' + author);
+		// 		} else {
+		// 			$('#author').text('- Unknown');
+		// 		}
+		// 	}
+		// });
+	}
+
+	// getNewQuote();
+
+	// $('.get-quote').on('click', function(event){
+	// 	event.preventDefault();
+	// 	getNewQuote();
+	// });
+
+	// $('.share-quote').on('click', function(event){
+	// 	event.preventDefault();
+	// 	window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote + ' -- ' + author));
+	// });
 
 });
 
